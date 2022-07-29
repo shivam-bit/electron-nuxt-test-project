c179b77a316e6602d1fabfe306e2f292036845e2
shivam-bit <shivama5tronot@gmail.com>
2022-07-29 14:35:47 +0530

    added electron autoupdater

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { app } from 'electron'
+import { dialog,app } from 'electron'
 const { autoUpdater } = require('electron-updater');
 
 // Quit when all windows are closed.
@@ -7,6 +7,64 @@ app.on('window-all-closed', function () {
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
+
+autoUpdater.on('checking-for-update',(event)=>{
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Application Checking for Update',
+  };
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    // if (response === 0) { 
+    //   autoUpdater.quitAndInstall();
+    // }
+  });
+})
+
+autoUpdater.on('update-available',(event)=>{
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Application Update Available',
+  };
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    // if (response === 0) { 
+    //   autoUpdater.quitAndInstall();
+    // }
+  });
+})
+
+autoUpdater.on('update-not-available',(event)=>{
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Application Update not Available!',
+  };
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    // if (response === 0) { 
+    //   autoUpdater.quitAndInstall();
+    // }
+  });
+})
+
+
+autoUpdater.on('update-downloaded', (info) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Update'],
+    title: 'Application Update',
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  };
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    if (response === 0) { 
+      autoUpdater.quitAndInstall();
+    }
+  });
+});
 
 autoUpdater.checkForUpdatesAndNotify();
 
